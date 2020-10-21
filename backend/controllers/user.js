36558c0628d524213b390d74fb811e0f4cfb7902
lukuenya <46133049+lukuenya@users.noreply.github.com>
2020-10-21 14:25:58 +0300

    Update user.js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,23 +1,103 @@
-const User = require("../models/user");
+const _ = require('lodash');
+const User = require('../models/user');
+const formidable = require('formidable');
+const fs = require('fs');
 
 exports.userById = (req, res, next, id) => {
-    User.findById(id).exec((err, user) => {
-        if (err || !user) {
-            return res.status(400).json({
-                error: "User not found!"
-            });
-        }
-        // add profile object in req with user info
-        req.profile = user;
-        next();
-    });
+    User.findById(id)
+        // populate followers and following users array
+        .populate('following', '_id name')
+        .populate('followers', '_id name')
+        .exec((err, user) => {
+            if (err || !user) {
+                return res.status(400).json({
+                    error: 'User not found'
+                });
+            }
+            req.profile = user; // adds profile object in req with user info
+            next();
+        });
 };
 
 exports.hasAuthorization = (req, res, next) => {
-    const authorized = req.profile && req.auth && req.profile._id === req.auth._id;
+    let sameUser = req.profile && req.auth && req.profile._id == req.auth._id;
+    let adminUser = req.profile && req.auth && req.auth.role === 'admin';
+
+    const authorized = sameUser || adminUser;
+
+    // console.log("req.profile ", req.profile, " req.auth ", req.auth);
+    // console.log("SAMEUSER", sameUser, "ADMINUSER", adminUser);
+
     if (!authorized) {
         return res.status(403).json({
-            error: "User is not authorized to perform this action!"
+            error: 'User is not authorized to perform this action'
         });
     }
+    next();
+};
+
+exports.allUsers = (req, res) => {
+    User.find((err, users) => {
+        if (err) {
+            return res.status(400).json({
+                error: err
+            });
+        }
+        res.json(users);
+    }).select('name email updated created role');
+};
+
+exports.getUser = (req, res) => {
+    req.profile.hashed_password = undefined;
+    req.profile.salt = undefined;
+    return res.json(req.profile);
+};
+
+exports.updateUser = (req, res, next) => {
+    let form = new formidable.IncomingForm();
+    // console.log("incoming form data: ", form);
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Photo could not be uploaded'
+            });
+        }
+        // save user
+        let user = req.profile;
+        // console.log("user in update: ", user);
+        user = _.extend(user, fields);
+
+        user.updated = Date.now();
+        // console.log("USER FORM DATA UPDATE: ", user);
+
+        if (files.photo) {
+            user.photo.data = fs.readFileSync(files.photo.path);
+            user.photo.contentType = files.photo.type;
+        }
+
+        user.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: err
+                });
+            }
+            user.hashed_password = undefined;
+            user.salt = undefined;
+            // console.log("user after update with formdata: ", user);
+            res.json(user);
+        });
+    });
+};
+
+exports.deleteUser = (req, res, next) => {
+    let user = req.profile;
+    user.remove((err, user) => {
+        if (err) {
+            return res.status(400).json({
+                error: err
+            });
+        }
+        res.json({ message: 'User deleted successfully' });
+    });
 };
