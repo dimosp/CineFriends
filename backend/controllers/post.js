327ec56a8798d3dc0b0d68794cb6b775492154c1
lukuenya <46133049+lukuenya@users.noreply.github.com>
2020-10-21 14:23:04 +0300

    Update post.js

---
+++ backend/controllers/post.js
@@ -0,0 +1,130 @@
+const Post = require('../models/post');
+const formidable = require('formidable');
+const fs = require('fs');
+const lodash = require('lodash');
+
+exports.postById = (req, res, next, id) => {
+    Post.findById(id)
+        .populate('postedBy', '_id name')
+        .populate('comments.postedBy', '_id name')
+        .populate('postedBy', '_id name role')
+        .select('_id title body created likes comments photo')
+        .exec((err, post) => {
+            if (err || !post) {
+                return res.status(400).json({
+                    error: err
+                });
+            }
+            req.post = post;
+            next();
+        });
+};
+
+/*
+exports.getPosts = (req, res) => {
+    const posts = Post.find()
+        .populate("postedBy", "_id name")
+        .populate("comments", "text created")
+        .populate("comments.postedBy", "_id name")
+        .select("_id title body created likes")
+        .sort({ created: -1 })
+        .then(posts => {
+            res.json(posts);
+        })
+        .catch(err => console.log(err));
+};
+*/
+
+// with pagination
+exports.getPosts = async (req, res) => {
+    // get current page from req.query or use default value of 1
+    const currentPage = req.query.page || 1;
+    // return 3 posts per page
+    const perPage = 6;
+    let totalItems;
+
+    const posts = await Post.find()
+        // countDocuments() gives you total count of posts
+        .countDocuments()
+        .then(count => {
+            totalItems = count;
+            return Post.find()
+                .skip((currentPage - 1) * perPage)
+                .populate('comments', 'text created')
+                .populate('comments.postedBy', '_id name')
+                .populate('postedBy', '_id name')
+                .select('_id title body created likes')
+                .limit(perPage)
+                .sort({ created: -1 });
+        })
+        .then(posts => {
+            res.status(200).json(posts);
+        })
+        .catch(err => console.log(err));
+};
+
+exports.createPost = (req, res) => {
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Image could not be uploaded'
+            });
+        }
+        let post = new Post(fields);
+
+        req.profile.hashed_password = undefined;
+        req.profile.salt = undefined;
+        post.postedBy = req.profile;
+
+        if (files.photo) {
+            post.photo.data = fs.readFileSync(files.photo.path);
+            post.photo.contentType = files.photo.type;
+        }
+        post.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: err
+                });
+            }
+            res.json(result);
+        });
+    });
+};
+
+exports.postsByUser = (req, res) => {
+    Post.find({ postedBy: req.profile._id })
+        .populate('postedBy', '_id name')
+        .select('_id title body created likes')
+        .sort('_created')
+        .exec((err, posts) => {
+            if (err) {
+                return res.status(400).json({
+                    error: err
+                });
+            }
+            res.json(posts);
+        });
+};
+
+exports.isPoster = (req, res, next) => {
+    let sameUser = req.post && req.auth && req.post.postedBy._id == req.auth._id;
+    let adminUser = req.post && req.auth && req.auth.role === 'admin';
+
+    // console.log("req.post ", req.post, " req.auth ", req.auth);
+    // console.log("SAMEUSER: ", sameUser, " ADMINUSER: ", adminUser);
+
+    let isPoster = sameUser || adminUser;
+
+    if (!isPoster) {
+        return res.status(403).json({
+            error: 'User is not authorized'
+        });
+    }
+    next();
+};
+
+exports.singlePost = (req, res) => {
+    return res.json(req.post);
+};
