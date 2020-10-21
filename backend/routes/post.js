061a009ccefee92b0876c4f46f2fca01362d21a4
lukuenya <46133049+lukuenya@users.noreply.github.com>
2020-10-21 14:34:43 +0300

    Update post.js

---
+++ backend/routes/post.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const {
+    getPosts,
+    createPost,
+    postsByUser,
+    postById,
+    singlePost,
+} = require('../controllers/post');
+const { requireSignin } = require('../controllers/auth');
+const { userById } = require('../controllers/user');
+const { createPostValidator } = require('../validator');
+
+const router = express.Router();
+
+router.get('/posts', getPosts);
+
+// post routes
+router.post('/post/new/:userId', requireSignin, createPost, createPostValidator);
+router.get('/posts/by/:userId', requireSignin, postsByUser);
+router.get('/post/:postId', singlePost);
+
+
+// any route containing :userId, our app will first execute userById()
+router.param('userId', userById);
+// any route containing :postId, our app will first execute postById()
+router.param('postId', postById);
+
+module.exports = router;
