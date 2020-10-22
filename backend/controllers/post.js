705dbfd1420eb4cbfe29cfb712df3e143290693c
lukuenya <46133049+lukuenya@users.noreply.github.com>
2020-10-22 18:13:58 +0300

    simple post controller

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,14 +1,9 @@
 const Post = require('../models/post');
-const formidable = require('formidable');
-const fs = require('fs');
-const lodash = require('lodash');
 
 exports.postById = (req, res, next, id) => {
     Post.findById(id)
         .populate('postedBy', '_id name')
-        .populate('comments.postedBy', '_id name')
-        .populate('postedBy', '_id name role')
-        .select('_id title body created likes comments photo')
+        .select('_id title body created ')
         .exec((err, post) => {
             if (err || !post) {
                 return res.status(400).json({
@@ -24,9 +19,7 @@ exports.postById = (req, res, next, id) => {
 exports.getPosts = (req, res) => {
     const posts = Post.find()
         .populate("postedBy", "_id name")
-        .populate("comments", "text created")
-        .populate("comments.postedBy", "_id name")
-        .select("_id title body created likes")
+        .select("_id title body created ")
         .sort({ created: -1 })
         .then(posts => {
             res.json(posts);
@@ -34,55 +27,8 @@ exports.getPosts = (req, res) => {
         .catch(err => console.log(err));
 };
 
-
- /*             // with pagination
-exports.getPosts = async (req, res) => {
-    // get current page from req.query or use default value of 1
-    const currentPage = req.query.page || 1;
-    // return 3 posts per page
-    const perPage = 6;
-    let totalItems;
-
-    const posts = await Post.find()
-        // countDocuments() gives you total count of posts
-        .countDocuments()
-        .then(count => {
-            totalItems = count;
-            return Post.find()
-                .skip((currentPage - 1) * perPage)
-                .populate('comments', 'text created')
-                .populate('comments.postedBy', '_id name')
-                .populate('postedBy', '_id name')
-                .select('_id title body created likes')
-                .limit(perPage)
-                .sort({ created: -1 });
-        })
-        .then(posts => {
-            res.status(200).json(posts);
-        })
-        .catch(err => console.log(err));
-};
-*/
-
 exports.createPost = (req, res) => {
-    let form = new formidable.IncomingForm();
-    form.keepExtensions = true;
-    form.parse(req, (err, fields, files) => {
-        if (err) {
-            return res.status(400).json({
-                error: 'Image could not be uploaded'
-            });
-        }
-        let post = new Post(fields);
-
-        req.profile.hashed_password = undefined;
-        req.profile.salt = undefined;
-        post.postedBy = req.profile;
-
-        if (files.photo) {
-            post.photo.data = fs.readFileSync(files.photo.path);
-            post.photo.contentType = files.photo.type;
-        }
+    const post = new Post(req.body);
         post.save((err, result) => {
             if (err) {
                 return res.status(400).json({
@@ -91,13 +37,12 @@ exports.createPost = (req, res) => {
             }
             res.json(result);
         });
-    });
 };
 
 exports.postsByUser = (req, res) => {
     Post.find({ postedBy: req.profile._id })
         .populate('postedBy', '_id name')
-        .select('_id title body created likes')
+        .select('_id title body created ')
         .sort('_created')
         .exec((err, posts) => {
             if (err) {
@@ -111,12 +56,7 @@ exports.postsByUser = (req, res) => {
 
 exports.isPoster = (req, res, next) => {
     let sameUser = req.post && req.auth && req.post.postedBy._id == req.auth._id;
-    let adminUser = req.post && req.auth && req.auth.role === 'admin';
-
-    // console.log("req.post ", req.post, " req.auth ", req.auth);
-    // console.log("SAMEUSER: ", sameUser, " ADMINUSER: ", adminUser);
-
-    let isPoster = sameUser || adminUser;
+    let isPoster = sameUser;
 
     if (!isPoster) {
         return res.status(403).json({
