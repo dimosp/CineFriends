89a62ee278e6cff0a2d04f0c5f722b2787b2ef61
kostasp-skg <konpap1996@hotmail.com>
2020-10-23 13:50:18 +0300

    Change code in posts endpoint
    Reviewed the code and fixed some bugs.

--- backend/validator/index.js
+++ backend/validator/index.js
@@ -62,4 +62,24 @@ exports.userSigninValidator = (req, res, next) => {
     }
     
     next();
+
 };
+
+exports.createPostValidator = (req, res, next) => {
+    // body
+    req.check('body', 'You must write a body for user post!').notEmpty();
+    req.check('body', 'Body must be between 4 to 2000 characters!').isLength({
+        min: 4,
+        max: 2000
+    });
+    // check for errors
+    const errors = req.validationErrors();
+    // if error show the first one as they happen
+    if (errors) {
+        const firstError = errors.map(error => error.msg)[0];
+        return res.status(400).json({ error: firstError });
+    }
+    // proceed to next middleware
+    next();
+};
+
