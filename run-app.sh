d997729df370b6f5077912f17d57b772a4aa28db
kostasp-skg <72674792+kostasp-skg@users.noreply.github.com>
2020-10-30 19:28:20 +0200

    Update run-app.sh
    Fixed code style

--- run-app.sh
+++ run-app.sh
@@ -34,34 +34,34 @@ while getopts ":hp:" option; do
          exit;;
       p) # give the path to the route directory
          backend_path="${2}/backend"
-		 frontend_package_path="${2}/frontend"
-		 frontend_app_path="${2}/frontend/src"
-		 
-		 # Install backend dependencies (this migth take a few minutes)
-		 cd $backend_path
-		 npm install				# comment this line if you have everything installed
-		 # Run Node.js server in backend
-		 # Accesible from http://localhost:8080/api/
-		 npm run dev &
-		 
-		 # This trailing ampersand directs the shell to run the command in the background, 
-		 # that is, it is forked and run in a separate sub-shell, as a job, asynchronously. 
-		 # The shell will immediately return the return status of 0 for true and continue 
-		 # as normal, either processing further commands in a script or returning the cursor 
-		 # focus back to the user in a Linux terminal.
-		 
-		 # Install frontend dependencies (this migth take a few minutes)
-		 cd ..
-       cd $frontend_package_path
-		 npm install &				# comment this line if you have everything installed
-		 # Run React.js app
-		 # Accesible from http://localhost:3000
-       cd ..
-		 cd $frontend_app_path
-		 npm start
+	 frontend_package_path="${2}/frontend"
+	 frontend_app_path="${2}/frontend/src"
+
+	 # Install backend dependencies (this migth take a few minutes)
+	 cd $backend_path
+	 npm install				# comment this line if you have everything installed
+	 # Run Node.js server in backend
+	 # Accesible from http://localhost:8080/api/
+	 npm run dev &
+
+	 # This trailing ampersand directs the shell to run the command in the background, 
+	 # that is, it is forked and run in a separate sub-shell, as a job, asynchronously. 
+	 # The shell will immediately return the return status of 0 for true and continue 
+	 # as normal, either processing further commands in a script or returning the cursor 
+	 # focus back to the user in a Linux terminal.
+
+	 # Install frontend dependencies (this migth take a few minutes)
+	 cd ..
+         cd $frontend_package_path
+	 npm install &				# comment this line if you have everything installed
+	 # Run React.js app
+	 # Accesible from http://localhost:3000
+         cd ..
+	 cd $frontend_app_path
+	 npm start
 		 
          exit;;
-	  \?) # incorrect option
+      \?) # incorrect option
          echo "Error: Invalid option"
          exit;;
    esac
