106d1a805529401fdc4c6f4d7c05886e183108f2
KAUTH <konpap1996@yahoo.com>
2020-11-17 12:52:20 +0200

    Fix posts not being sent
    Changed the API call that was made in the
    front-end to create a new post for the user
    (it had a bug previously).

--- frontend/src/post/apiPost.js
+++ frontend/src/post/apiPost.js
@@ -4,23 +4,21 @@
 //     "password": "Deadpooler1"
 //   }
 
-export const create = (userId, token, post) => {
+export const create = (userId, token, postData) => {
     return fetch(`${process.env.REACT_APP_API_URL}/posts/new/${userId}`, {
         method: 'POST',
         headers: {
             Accept: "application/json",
             Authorization: `Bearer ${token}`,
-            'Content-Type': 'application/json'
         },
-        body: JSON.stringify({
-            body: post.get("body")
-        })
+        body: postData
     })
         .then(response => {
-            return response;
+            return response.json();
         })
         .catch(err => console.log(err));
 };
+
 
 // fetch(`${process.env.REACT_APP_API_URL}/post/new/${userId}`, {
 // fetch(`${process.env.REACT_APP_API_URL}/posts`
